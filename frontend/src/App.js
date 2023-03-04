c11822011efc261841db314879a6f2a8b06118b8
Sanketh Gunasekara <70938689+Sankethg0@users.noreply.github.com>
2023-03-04 09:18:48 +0530

    Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import React,{useEffect,createContext,useReducer,useContext} from 'react'
+import {BrowserRouter,Routes,Route,useNavigate} from 'react-router-dom'
+import {initialState, reducer} from './reducers/userReducer';
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Home from './pages/Home'
+
+export const userContext = createContext();
+
+const Routing = () =>{
+  const navigate = useNavigate();
+  const {state,dispatch} = useContext(userContext);
+  useEffect (()=>{
+      const user = JSON.parse(localStorage.getItem('user'));
+      if(user){
+        dispatch({type:'USER',payload:user});
+      }else{
+        navigate('/login');
+      }
+  },[])
+  return(
+    <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+  )
+}
+
+function App() {
+  const [state,dispatch] = useReducer(reducer,initialState)
+  return (
+    <userContext.Provider value ={{state,dispatch}}>
+      <BrowserRouter>
+        <Routing/>
+      </BrowserRouter>
+    </userContext.Provider>
+  );
+}
+
+export default App;
